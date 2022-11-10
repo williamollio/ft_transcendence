169e4309fe2365ed611ba1733323ba14fcb74f1c
William <williamollio98@gmail.com>
2022-11-10 21:01:34 +0100

    article endpoints configured

--- nest/src/articles/articles.controller.ts
+++ nest/src/articles/articles.controller.ts
@@ -2,33 +2,52 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import {ApiCreatedResponse, ApiOkResponse, ApiTags} from "@nestjs/swagger";
+import {ArticleEntity} from "./entities/article.entity";
 
 @Controller('articles')
+@ApiTags('articles')
+
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
-  @Post()
-  create(@Body() createArticleDto: CreateArticleDto) {
-    return this.articlesService.create(createArticleDto);
-  }
-
   @Get()
+  @ApiOkResponse({type : ArticleEntity, isArray : true})
   findAll() {
     return this.articlesService.findAll();
   }
 
+  @Get('drafts')
+  @ApiOkResponse({type : ArticleEntity, isArray : true})
+  findDrafts() {
+    return this.articlesService.findDrafts();
+  }
+
   @Get(':id')
+  @ApiOkResponse({type : ArticleEntity})
   findOne(@Param('id') id: string) {
+    // + operator casts to a number
     return this.articlesService.findOne(+id);
   }
 
+  @Post()
+  @ApiCreatedResponse({type : ArticleEntity})
+  create(@Body() createArticleDto: CreateArticleDto) {
+    return this.articlesService.create(createArticleDto);
+  }
+
   @Patch(':id')
+  @ApiOkResponse({type : ArticleEntity})
   update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
     return this.articlesService.update(+id, updateArticleDto);
   }
 
+
   @Delete(':id')
+  @ApiOkResponse({type : ArticleEntity})
   remove(@Param('id') id: string) {
     return this.articlesService.remove(+id);
   }
+
+
 }
--- nest/src/articles/articles.service.ts
+++ nest/src/articles/articles.service.ts
@@ -8,7 +8,11 @@ export class ArticlesService {
   constructor(private prisma: PrismaService) {}
 
   create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
+    return this.prisma.article.create({ data : createArticleDto });
+  }
+
+  findDrafts() {
+    return this.prisma.article.findMany({ where: { published: true } });
   }
 
   findAll() {
@@ -16,14 +20,15 @@ export class ArticlesService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} article`;
+    return this.prisma.article.findUnique({ where : {id} })
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
+    return this.prisma.article.update({ where: {id}, data : updateArticleDto })
   }
 
   remove(id: number) {
-    return `This action removes a #${id} article`;
+    return this.prisma.article.delete({where : {id}})
   }
+
 }
