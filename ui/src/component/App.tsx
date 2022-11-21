535edaa7801b1c9e4c1edeee3426fc8243882ab1
William <williamollio98@gmail.com>
2022-11-21 18:08:34 +0100

    setting react app 2

---
+++ ui/src/component/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Edit
+        </p>
+    </div>
+  );
+}
+
+export default App;
