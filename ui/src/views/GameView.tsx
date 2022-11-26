a9b3e4f7a5b3528605f30a7e37feda4a7b603a0f
William <williamollio98@gmail.com>
2022-11-26 23:20:15 +0100

    init game view

---
+++ ui/src/views/GameView.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../component/Navbar";
+
+export default function LoginView(): React.ReactElement {
+  return (
+    <>
+      <Navbar />
+      <div>Game</div>
+    </>
+  );
+}
