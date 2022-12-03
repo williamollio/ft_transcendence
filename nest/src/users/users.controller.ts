1a3c6c0d15d5a3f67b2c97c706c4945a28833dbb
William <williamollio98@gmail.com>
2022-12-03 23:56:19 +0100

    User POST handle bad request

--- nest/src/users/users.controller.ts
+++ nest/src/users/users.controller.ts
@@ -6,12 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { UserEntity } from './entities/user.entity';
+import { User } from '@prisma/client';
 
 @Controller('users')
 @ApiTags('users')
@@ -20,32 +23,40 @@ export class UsersController {
 
   @Get()
   @ApiOkResponse({ type: UserEntity, isArray: true })
-  findAll() {
+  public async findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
   @ApiOkResponse({ type: UserEntity })
-  findOne(@Param('id') id: string) {
+  public async findOne(@Param('id') id: string) {
     // + operator casts to a number
     return this.usersService.findOne(+id);
   }
 
   @Post()
   @ApiCreatedResponse({ type: UserEntity })
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  public async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    try {
+      return await this.usersService.create(createUserDto);
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'This username already exists',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
   @Patch(':id')
   @ApiOkResponse({ type: UserEntity })
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  public async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
   @ApiOkResponse({ type: UserEntity })
-  remove(@Param('id') id: string) {
+  public async remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
 }
