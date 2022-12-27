826dd4b1daf8060d6868083fa1176cd664353a7f
William <williamollio98@gmail.com>
2022-12-27 21:46:46 +0100

    set up i18n

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -4,7 +4,14 @@ import "./index.css";
 import App from "./component/App";
 import { BrowserRouter } from "react-router-dom";
 import { TranscendanceProvider } from "./context/transcendance-context";
+import { initReacti18n } from "./i18n/i18n";
 
+// Initialize i18n
+async function main() {
+  if (typeof window !== "undefined") await initReacti18n();
+}
+
+main();
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
