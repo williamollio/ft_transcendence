82bd154e0763bbde027b66622eeaac577fe0ba26
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-12-31 16:33:44 +0100

    Added OAuth URI

--- nest/src/auth/auth/auth.service.ts
+++ nest/src/auth/auth/auth.service.ts
@@ -3,7 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   public async getAuthURL(): Promise<string> {
-    // TODO: Create URI from .env file
-    return 'TODO';
+    let url = 'https://api.intra.42.fr/oauth/authorize?';
+    url += 'client_id=' + process.env.CLIENT_ID;
+    url += '&redirect_uri=' + process.env.REDIRECT_URI;
+    url += '&scope=public';
+    return url;
   }
 }
