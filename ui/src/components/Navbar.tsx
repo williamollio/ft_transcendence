f29c308652c15afe0864242bc9928c91b7ac7b9a
William <williamollio98@gmail.com>
2023-01-22 21:36:15 +0100

    get picture from the store

--- ui/src/components/Navbar.tsx
+++ ui/src/components/Navbar.tsx
@@ -5,11 +5,20 @@ import { idTabs } from "../interfaces/tab.interface";
 import { tabs, NavTab, TabWithId } from "../interfaces/tab.interface";
 import { makeStyles } from "tss-react/mui";
 import { useLocation } from "react-router-dom";
+import classes from "../styles.module.scss";
+import { useImageStore } from "../store/users-store";
+import Avatar from "@material-ui/core/Avatar";
+import Box from "@mui/material/Box";
 
 export default function NavBar(): React.ReactElement {
   const state = useLocation().state;
   const navigate = useNavigate();
   const { classes } = useStyles();
+
+  const [image, setImage] = useImageStore((state) => [
+    state.image,
+    state.setImage,
+  ]);
 
   const [selectedTabId, setSelectedTabId] = useState<number>(
     state?.activeTabId ?? idTabs.PROFILE
@@ -41,35 +50,62 @@ export default function NavBar(): React.ReactElement {
   }
 
   return (
-    <AppBar>
-      <Tabs
-        value={selectedTabId}
-        indicatorColor="secondary"
-        textColor="secondary"
-        className={classes.menuBar}
-        centered={true}
-        TabIndicatorProps={{
-          style: { marginBottom: "5px", marginLeft: "-8px" },
-        }}
-      >
-        {tabsWithId.map((tab) => {
-          return (
-            <Tab
-              key={`navbar-${tab.id}`}
-              label={tab.label}
-              value={tab.id}
-              classes={{
-                root: classes.tab,
+    <>
+      <AppBar className={classes.menuBar}>
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Tabs
+            value={selectedTabId}
+            indicatorColor="secondary"
+            textColor="secondary"
+            sx={{ width: "96%", marginLeft: "65px" }} // should be equal as the width of container-picture
+            centered={true}
+            TabIndicatorProps={{
+              style: { marginBottom: "5px", marginLeft: "-8px" },
+            }}
+          >
+            {tabsWithId.map((tab) => {
+              return (
+                <Tab
+                  key={`navbar-${tab.id}`}
+                  label={tab.label}
+                  value={tab.id}
+                  classes={{
+                    root: classes.tab,
+                  }}
+                  onClick={() => {
+                    navigateToSelectedTab(tab.id);
+                  }}
+                  className={classes.tab}
+                />
+              );
+            })}
+          </Tabs>
+          <Box
+            id="container-picture"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              width: "4%",
+            }}
+          >
+            <Avatar
+              style={{
+                width: "55px",
+                height: "55px",
               }}
-              onClick={() => {
-                navigateToSelectedTab(tab.id);
-              }}
-              className={classes.tab}
+              src={image ? URL.createObjectURL(image) : ""}
             />
-          );
-        })}
-      </Tabs>
-    </AppBar>
+          </Box>
+        </Box>
+      </AppBar>
+    </>
   );
 }
 
@@ -77,10 +113,13 @@ const useStyles = makeStyles()(() => ({
   menuBar: {
     height: "4rem",
     width: "100%",
-    backgroundColor: "#1d3c45",
+    backgroundColor: classes.colorPrimary,
+    display: "flex",
+    alignItems: "center",
+    flexWrap: "wrap",
   },
   tab: {
-    color: "#fff1e1",
+    color: classes.colorSecondary,
     fontSize: "1rem",
     fontFamily: "Soin",
     fontWeight: "bold",
