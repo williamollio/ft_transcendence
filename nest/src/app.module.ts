fd35ac07b9bd7ebaedc953b7e6e2696d2ef76c55
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-01-24 11:00:02 +0100

    Corrected hierarchy in `auth` folder

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [PrismaModule, UsersModule, AuthModule],
