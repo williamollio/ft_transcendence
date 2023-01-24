7a0519d3190a616e67f58f8ba7342f6e96f78640
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-01-24 17:35:38 +0100

    Fixed imports

--- nest/src/auth/auth.module.ts
+++ nest/src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [JwtModule, UsersModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- nest/src/users/users.module.ts
+++ nest/src/users/users.module.ts
@@ -9,5 +9,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
   controllers: [UsersController],
   providers: [UsersService],
   imports: [PrismaModule],
+  exports: [UsersService],
 })
 export class UsersModule {}
