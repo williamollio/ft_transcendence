e4c8dc3e3a1669eee65752ff316da2a952794e70
William <williamollio98@gmail.com>
2023-01-24 21:34:11 +0100

    image in store might be of type Blob

--- ui/src/store/users-store.ts
+++ ui/src/store/users-store.ts
@@ -3,14 +3,14 @@ import { Dispatch, SetStateAction } from "react";
 
 // describes the shape of the state
 interface ImageStore {
-  image: File | null;
-  setImage: (img: File) => void;
+  image: File | Blob |  null;
+  setImage: (img: File | Blob) => void;
 }
 // Dispatch<SetStateAction<ImageStore>>
 // allows the TypeScript compiler to infer the correct type of the state
 export const useImageStore = create<ImageStore>(
   (set: Dispatch<SetStateAction<ImageStore>>) => ({
     image: null,
-    setImage: (img: File) => set((state) => ({ ...state, image: img })),
+    setImage: (img: File | Blob) => set((state) => ({ ...state, image: img })),
   })
 );
