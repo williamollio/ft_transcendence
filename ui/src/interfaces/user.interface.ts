cc35e4832e55b92b41318da594a6727f5726afef
William <williamollio98@gmail.com>
2023-01-26 22:53:27 +0100

    interface user updt

--- ui/src/interfaces/user.interface.ts
+++ ui/src/interfaces/user.interface.ts
@@ -1,12 +1,16 @@
-export interface User {
-  id: number;
-  name: string;
-}
-
 export interface Friends {
   id: number;
 }
 export interface UserCreation {
   name: string;
   friends?: Friends[];
+}
+
+export interface User {
+  id: number;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+  filename?: string;
+  friends?: User[];
 }
