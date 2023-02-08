42a3c4455e290890bfa2d84df4c71ff20dfab66b
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-02-08 19:13:50 +0100

    Added refresh token to the DB

--- nest/src/users/entities/user.entity.ts
+++ nest/src/users/entities/user.entity.ts
@@ -28,6 +28,9 @@ export class UserEntity implements User {
   updatedAt: Date;
 
   @ApiProperty()
+  refreshToken: string | null;
+
+  @ApiProperty()
   filename: string | null;
 
   @ApiProperty()
