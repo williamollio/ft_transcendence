766bb7125b25058530f4e0dc49b8679f9dbd3159
William <williamollio98@gmail.com>
2023-02-21 21:37:16 +0100

    set header in axios instance

--- ui/src/services/common/axios-instance.ts
+++ ui/src/services/common/axios-instance.ts
@@ -1,6 +1,10 @@
+import { Cookie } from "../../utils/auth-helper";
 import axios from "axios";
 import { getBaseUrl } from "../../utils/url-helper";
 
 export const axiosInstance = axios.create({
   baseURL: `${getBaseUrl()}`,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem(Cookie.TOKEN)}`,
+  },
 });
