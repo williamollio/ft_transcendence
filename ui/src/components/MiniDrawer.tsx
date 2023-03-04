6fbba7db953134633109c7cd455cfdc389893ab1
William <williamollio98@gmail.com>
2023-03-04 17:39:34 +0100

    mini drawer fetch friends

--- ui/src/components/MiniDrawer.tsx
+++ ui/src/components/MiniDrawer.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
+import { Response } from "../services/common/resolve";
 import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
@@ -10,6 +11,10 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import { Avatar, Divider } from "@mui/material";
+import { UserIds } from "../interfaces/user.interface";
+import { Cookie, getTokenData } from "../utils/auth-helper";
+import friendshipsService from "../services/friendships.service";
 
 const drawerWidth = 240;
 
@@ -58,6 +63,25 @@ const Drawer = styled(MuiDrawer, {
 export default function MiniDrawer() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
+  const [friends, setFriends] = React.useState<UserIds[]>([]);
+  const [userId, setUserId] = React.useState<string>("");
+
+  React.useEffect(() => {
+    let token = localStorage.getItem(Cookie.TOKEN);
+
+    if (token) {
+      setUserId(getTokenData(token).id.toString());
+    }
+    if (userId) {
+      fetchFriends();
+    }
+  }, [userId]);
+
+  async function fetchFriends() {
+    const usersFriends: Response<UserIds[]> =
+      await friendshipsService.getAccepted(userId);
+    setFriends(usersFriends.data);
+  }
 
   const triggerDrawerOpen = () => {
     setOpen(!open);
@@ -67,33 +91,37 @@ export default function MiniDrawer() {
     <Box sx={{ display: "flex" }}>
       <Drawer variant="permanent" open={open}>
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map(
-            // TODO : render friends
-            (text, index) => (
-              <ListItem key={text} disablePadding sx={{ display: "block" }}>
-                <ListItemButton
-                  onClick={triggerDrawerOpen}
+          {friends.map((friend, index) => (
+            <ListItem key={index} disablePadding sx={{ display: "block" }}>
+              <ListItemButton
+                onClick={triggerDrawerOpen}
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? "initial" : "center",
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
                   sx={{
-                    minHeight: 48,
-                    justifyContent: open ? "initial" : "center",
-                    px: 2.5,
+                    minWidth: 0,
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
                   }}
                 >
-                  <ListItemIcon
-                    sx={{
-                      minWidth: 0,
-                      mr: open ? 3 : "auto",
-                      justifyContent: "center",
-                    }}
-                  >
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-                </ListItemButton>
-              </ListItem>
-            )
-          )}
+                  <Avatar />
+                </ListItemIcon>
+                <ListItemText primary={index} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
         </List>
+        <Divider
+          variant="middle"
+          sx={{
+            marginTop: "2.5rem",
+            marginBottom: "2.5rem",
+          }}
+        ></Divider>
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}></Box>
     </Box>
