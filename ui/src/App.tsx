f5861bc16a54f39a85d0ead569b536d2d35b22ee
hskowron <hskowron@student.42heilbronn.de>
2023-03-19 19:55:26 +0100

    fixed vanishing channels when refreshing the page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -40,6 +40,7 @@ export default function App() {
       }
       if (channelSocket.socket.connected === false) {
         channelSocket.socket.auth = { token: token };
+		channelSocket.initializeName(token);
         channelSocket.socket.connect();
       }
     }
