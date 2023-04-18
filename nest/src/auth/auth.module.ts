1271b4c156711eddf77cc44ca9363144747d5af9
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-04-18 17:08:12 +0200

    Removed JWT refreshing in its entirety

--- nest/src/auth/auth.module.ts
+++ nest/src/auth/auth.module.ts
@@ -6,7 +6,6 @@ import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { Intra42Strategy } from './strategies/intra42.strategy';
 import * as process from 'process';
-import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
 import { FullAuthStrategy } from './strategies/full-auth-strategy';
 
 @Module({
@@ -22,7 +21,6 @@ import { FullAuthStrategy } from './strategies/full-auth-strategy';
     AuthService,
     JwtStrategy,
     Intra42Strategy,
-    JwtRefreshStrategy,
     FullAuthStrategy,
   ],
 })
--- nest/src/auth/auth.service.ts
+++ nest/src/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import {
   BadRequestException,
-  ForbiddenException,
   HttpStatus,
   Injectable,
   InternalServerErrorException,
@@ -11,7 +10,6 @@ import { UsersService } from '../users/users.service';
 import { JwtUser } from '../users/interface/jwt-user.interface';
 import { Intra42User } from '../users/interface/intra42-user.interface';
 import * as process from 'process';
-import * as argon2 from 'argon2';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { authenticator } from 'otplib';
 import { toDataURL } from 'qrcode';
@@ -25,21 +23,10 @@ export class AuthService {
   ) {}
 
   private async generateTokens(payload: JwtUser) {
-    const [accessToken, refreshToken] = await Promise.all([
-      this.jwtService.signAsync(payload, {
-        secret: process.env.JWT_SECRET,
-        // expiresIn: '15m', // TODO : William set in FE
-      }),
-      this.jwtService.signAsync(payload, {
-        secret: process.env.JWT_REFRESH_SECRET,
-        // expiresIn: '7d', // TODO : William set in FE
-      }),
-    ]);
-
-    return {
-      accessToken,
-      refreshToken,
-    };
+    return await this.jwtService.signAsync(payload, {
+      secret: process.env.JWT_SECRET,
+      // expiresIn: '15m', // TODO : William set in FE
+    });
   }
 
   async signIn(user: Intra42User) {
@@ -77,11 +64,6 @@ export class AuthService {
       intraId: foundUser.intraId,
     });
 
-    await this.userService.updateRefreshToken(
-      foundUser.id,
-      await argon2.hash(tokens.refreshToken),
-    );
-
     await this.userService.set2FALogged(foundUser.id, false);
 
     return tokens;
@@ -94,23 +76,6 @@ export class AuthService {
       console.error(e);
       throw new InternalServerErrorException();
     }
-  }
-
-  async refreshTokens(userId: string, refreshToken: string) {
-    const user = await this.userService.findOne(userId);
-    if (!user || !user.refreshToken)
-      throw new ForbiddenException('Access denied');
-    if (!(await argon2.verify(user.refreshToken, refreshToken)))
-      throw new ForbiddenException('Access denied');
-    const tokens = await this.generateTokens({
-      id: user.id,
-      intraId: user.intraId,
-    });
-    await this.userService.updateRefreshToken(
-      user.id,
-      await argon2.hash(tokens.refreshToken),
-    );
-    return tokens;
   }
 
   async enable2FA(userId: string): Promise<string> {
@@ -144,6 +109,5 @@ export class AuthService {
     const user = await this.userService.findOne(userId);
     if (!user) throw new UnauthorizedException('Unauthorized');
     await this.userService.set2FALogged(userId, false);
-    await this.userService.updateRefreshToken(user.id, '');
   }
 }
