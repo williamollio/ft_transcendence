7771dd5f5fcd3c6d9a56da7199015a88855e2855
William <williamollio98@gmail.com>
2023-06-01 19:11:24 +0200

    baseUrl

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ft_transcendence/",
   server: {
     host: "0.0.0.0",
     port: 3000,
-    base: "/ft_transcendence/",
   },
 });
