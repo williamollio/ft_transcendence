7889fa0cd9ade57f7276d5e9d571002e67dd1e5a
William <williamollio98@gmail.com>
2023-08-22 15:08:34 +0200

    cors server

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,12 +1,15 @@
-// src/main.ts
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      credentials: true,
+      origin: process.env.DOMAIN,
+    },
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Median')
@@ -16,9 +19,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors({
-    origin: '*',
-  });
+
   app.use(cookieParser());
   await app.listen(8080);
 }
