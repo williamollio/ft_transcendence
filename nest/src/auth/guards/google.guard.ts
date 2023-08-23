227d50f79cd5d573d1b127d1c31a1c903d2a1fb9
William <williamollio98@gmail.com>
2023-08-23 14:01:23 +0200

    del

--- nest/src/auth/guards/google.guard.ts
+++ nest/src/auth/guards/google.guard.ts
@@ -2,11 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class GoogleGuard extends AuthGuard('google') {
-  //   async canActivate(context: ExecutionContext) {
-  //     const activate = (await super.canActivate(context)) as boolean;
-  //     const request = context.switchToHttp().getRequest();
-  //     await super.logIn(request);
-  //     return activate;
-  //   }
-}
+export class GoogleGuard extends AuthGuard('google') {}
